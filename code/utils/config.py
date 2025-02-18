39ab67f731d0ac8a4d6ee1a53e15ef6efc601a5f
leirubinstein <leilanierubinstein@gmail.com>
2025-02-18 07:38:52 +0000

    add buildings exploration

---
+++ code/utils/config.py
@@ -0,0 +1,28 @@
+#####################################################
+##################### USER 
+#####################################################
+
+user = "leilanie"
+# user = "ryan"
+# user = "josh"
+# user = "rachel"
+
+#####################################################
+##################### DIRECTORIES & FILE PATHS 
+#####################################################
+
+data_dir = "/capstone/wildfire_prep/data"
+repo_dir = f"/capstone/wildfire_prep/{user}/data-preparation"
+
+fig_dir = f"{repo_dir}/figures"
+basemap_dir = f"{data_dir}/basemaps"
+
+#####################################################
+##################### PARAMETERS
+#####################################################
+
+geodetic_crs = "EPSG:4326"
+mercator_crs = "EPSG:3857"
+mollweide_crs = "ESRI:54009"
+
+
